3517a845c9216b23f6926382a1898266f239f928
jumabai-2 <aiypov.jumabai.itiu.2@gmail.com>
2023-04-12 22:34:21 +0600

    accardionProgrammer

--- src/pages/TeamBuilding/TeamBuilding.js
+++ src/pages/TeamBuilding/TeamBuilding.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import CourseProgramme from "./CourseProgramme/CourseProgramme";
+import AccordionProgrammer from "./accordionProgrammer/AccordionProgrammer";
 
 const TeamBuilding = () => {
     return (
         <>
             <CourseProgramme/>
+            <AccordionProgrammer/>
        </>
     );
 };
