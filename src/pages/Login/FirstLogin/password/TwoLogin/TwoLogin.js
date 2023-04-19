df80688485f9a0f47e21f268e3f896a3f2c5de5e
jumabai-2 <aiypov.jumabai.itiu.2@gmail.com>
2023-04-19 22:31:30 +0600

    loginForget

--- src/pages/Login/FirstLogin/password/TwoLogin/TwoLogin.js
+++ src/pages/Login/FirstLogin/password/TwoLogin/TwoLogin.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Book from "../../../../../assets/img/logo.svg";
+import password from "../password";
+import {useNavigate} from "react-router-dom";
 
 const TwoLogin = () => {
+    const [forget, setForget] = useState("")
+    const [forgetOne, setForgetOne] = useState("")
+    const [forgotError , setForgotError] = useState(false)
+    const[error, setError] = useState('Пароли не совпадают!')
+    const navigate = useNavigate()
+
+function handleConfirmPassword() {
+    if (forget === forgetOne) {
+        // alert('Succes')
+        navigate('/')
+    } else {
+        setForgotError(true)
+    }
+}
+
     return (
         <div id='twoLogin'>
             <div className="container">
                 <div className="twoLogin">
                     <div className="twoLogin--text">
-                        <img src={Book} alt="" />
+                        <img src={Book} alt=""/>
                         <h2>SELF DEVELOPING SCHOOL</h2>
                         <div className="twoLogin--text__block">
                             <h3>Востановить пароль</h3>
-                            <input type="password" placeholder={'Новый пароль'}/>
-                            <input type="password" placeholder={'Повторите'}/>
-                            <button>Востановить пароль</button>
+                            {forgotError ? <p style={{
+                                color:"red",
+                                marginLeft:"-150px"
+                            }}>{error}</p> : null}
+
+                            <input type="password" placeholder={'Новый пароль'}
+                                   onChange={(e) => setForget(e.target.value)}
+                            />
+                            <input type="password" placeholder={'Повторите'}
+                                   onChange={(e) => setForgetOne(e.target.value)}/>
+                            <button onClick={handleConfirmPassword}>Востановить пароль</button>
                         </div>
                     </div>
                 </div>
